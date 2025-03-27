ef5c92153399bfd00628f2172a0ad625eb589e1c
DanielMarreroSanchez <daniel.marrero.sanchez@udc.es>
2025-03-27 11:31:27 +0100

    Update bid_stack.c

--- bid_stack.c
+++ bid_stack.c
@@ -12,34 +12,28 @@
 /* Write your code here... */
 
 void createemptystack(tStackL* S) {
-    *S=SNULL;
+    S->top = SNULL;
 }
 
 bool isemptystack(tStackL S) {
-    return (S==SNULL);
-}
-
-bool createnode(tPosS* p) {
-    *p=malloc(sizeof(struct tNodeS));
-    return (*p!=SNULL);
+    return (S.top == SNULL);
 }
 
 bool push(tItemS item, tStackL* S) {
-    tPosS aux;
-    if (!createnode(&aux)) {
+    if(S->top == SMAX-1) {
         return false;
-    }else {
-        aux->data=item;
-        aux->down=*S;
-        *S=aux;
-        return true;
+    }else{
+        S->top++;
+        S->data[S->top] = item;
     }
 }
 
-void pop(tStackL* S) {
-    tPosS aux;
-    *S=(*S)->down;
-    free(aux);
+void pop(tStackL* S){
+        S->top--;
+}
+
+tItemS peek(tStackL S) {
+    return S.data[S.top];
 }
 
 tItemS peek(tStackL S) {
