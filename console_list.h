748b76fe8d7b4c8c40b411ebf4481a1f321d0886
Iagoba-png <iago.alcoba@udc.es>
2025-03-27 11:06:00 +0100

    Principio

---
+++ console_list.h
@@ -0,0 +1,18 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 2
+ * AUTHOR 1: ***************************** LOGIN 1: **********
+ * AUTHOR 2: ***************************** LOGIN 2: **********
+ * GROUP: *.*
+ * DATE: ** / ** / **
+ */
+
+#ifndef CONSOLE_LIST_H
+#define CONSOLE_LIST_H
+
+#include "types.h"
+#include "bid_stack.h"
+
+/* Write your code here... */
+
+#endif
