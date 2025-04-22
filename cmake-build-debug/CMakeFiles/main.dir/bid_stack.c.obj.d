6e01eebfd0228c5873835cb4a4a5d23fb8087e6c
Iagoba-png <iago.alcoba@udc.es>
2025-04-22 14:05:12 +0200

    Funciones en main completas pero no guarda los items en la lista

---
+++ cmake-build-debug/CMakeFiles/main.dir/bid_stack.c.obj.d
@@ -0,0 +1,5 @@
+CMakeFiles/main.dir/bid_stack.c.obj: \
+ C:\Users\USUARIO\CLionProjects\P2_PROII\bid_stack.c \
+ C:\Users\USUARIO\CLionProjects\P2_PROII\bid_stack.h \
+ C:\Users\USUARIO\CLionProjects\P2_PROII\types.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.2/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdbool.h
